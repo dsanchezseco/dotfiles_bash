40d7cc01ee33df2e4ed3b6bf2e134140abcdaaf2
dsanchezseco <david.sanchez-seco@bbva.com>
2019-05-31 11:27:38 +0200

    added artifactory_creds

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-#TODO: move the files aws_load_session.sh, bash_aliases and git_branch_info.sh to $HOME (hide files with a dot `.` before the name)
+#TODO: move the files aws_load_session.sh, artifactory_creds, bash_aliases and git_branch_info.sh to $HOME (hide files with a dot `.` before the name)
 #TODO: concat concat2bashrc content to $HOME/.bashrc but controll that it can be done multiple times
+
+#OPTIONAL TODO: create and populate artifactory_creds file
