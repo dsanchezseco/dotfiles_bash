e78ca80c7c7099ff4be96897f2d5d652fe9e8b73
dsanchezseco <david.sanchez-seco@bbva.com>
2019-05-31 11:22:38 +0200

    renamed all and start install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#TODO: move the files aws_load_session.sh, bash_aliases and git_branch_info.sh to $HOME (hide files with a dot `.` before the name)
+#TODO: concat concat2bashrc content to $HOME/.bashrc but controll that it can be done multiple times
